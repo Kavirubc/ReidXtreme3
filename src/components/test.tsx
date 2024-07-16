6a93d9c61e84a45e12878be1cd145acb584efca6
Sandhavi Wanigasooriya <sandhaviwanigasooriya@gmail.com>
2024-07-16 16:05:20 +0530

    font test file

---
+++ src/components/test.tsx
@@ -0,0 +1,22 @@
+
+
+const Test = () => {
+    return (
+        <div className="flex flex-col h-screen items-center content-center justify-center text-slate-50 text-4xl">
+
+            <div className="font-des text-4xl pb-10">
+                Whereas recognition of the inherent dignity
+            </div>
+            <div className="font-zin text-4xl pb-10">
+                Whereas recognition of the inherent dignity
+            </div>
+            <div className="font-bruno text-4xl">
+                Whereas recognition of the inherent dignity
+            </div>
+        </div>
+
+
+    );
+};
+
+export default Test;
