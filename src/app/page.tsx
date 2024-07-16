3d2957c4ce6beebe664b5a724354401b8b8da239
lakshith <lakshith.k.nishshanke@gmail.com>
2024-07-16 12:11:44 +0530

    hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,26 @@
 'use client'
 
+import Image from "next/image"
 import './styles.scss'
 
 
 export default function Home() {
     return (
         <main>
-
+            <Image
+                src="/hero_bg.png"
+                alt="Vercel Logo"
+                layout='fill'
+                objectFit='cover' priority
+                className={"hero-bg"}
+            />
+            <Image
+                src="/logo.png"
+                alt="ReidExtreme Logo"
+                width={200}
+                height={200}
+                className={"logo"}
+            />
         </main>
     );
 }
