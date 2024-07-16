5555c0cf36ea0b10fe289d570a6bfc9444db52b5
Sandhavi Wanigasooriya <sandhaviwanigasooriya@gmail.com>
2024-07-16 16:03:37 +0530

    Add Next.js configuration files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,9 @@
 import type { Config } from "tailwindcss";
 
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -13,8 +17,49 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+
+      boxShadow: {
+        'custom-hover': '0 0 30px #008A5F',
+        input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
+      },
+      colors: {
+        primary: '#131313',
+        secondary: '#008A5F',
+        tertiary: '#062C20',
+      },
+      fontFamily: {
+        des: ['Raleway', 'sans-serif'],
+        zin: ['Zenzai Itachi', 'serif'],
+        bruno: ['BrunoAceSc', 'serif']
+      },
+      "animation": {
+        shimmer: "shimmer 2s linear infinite"
+      },
+      "keyframes": {
+        shimmer: {
+          from: {
+            "backgroundPosition": "0 0"
+          },
+          to: {
+            "backgroundPosition": "-200% 0"
+          }
+        }
+      }
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 };
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
 export default config;
+
+{
+}
