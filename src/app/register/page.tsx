c8aadd2a23c6fb38f426845edcc21732742e624a
Sandhavi Wanigasooriya <sandhaviwanigasooriya@gmail.com>
2024-07-18 14:27:40 +0530

    layout changed

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -5,12 +5,20 @@ import { useState } from 'react';
 const RegistrationForm = () => {
     const [formData, setFormData] = useState({
         teamName: '',
+        teamEmail: '',
         teamLeaderName: '',
         teamLeaderEmail: '',
-        contactNumber: '',
+        teamleaderscontactNumber: '',
+        university: '',
         teamMember1: '',
+        teamMember1email: '',
+        teamMember1contactNumber: '',
         teamMember2: '',
-        teamMember3: ''
+        teamMember2email: '',
+        teamMember2contactNumber: '',
+        teamMember3: '',
+        teamMember3email: '',
+        teamMember3contactNumber: ''
     });
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -30,7 +38,7 @@ const RegistrationForm = () => {
             className="flex flex-col items-center justify-center min-h-screen p-4 bg-black text-white font-sans pb-56"
         >
             <h2 className="text-3xl font-bruno mb-8 pt-5">Register Your Team</h2>
-            <form onSubmit={handleSubmit} className="w-full max-w-lg space-y-6 font-des tracking-wider">
+            <form onSubmit={handleSubmit} className="w-full max-w-5xl space-y-6 font-des tracking-widest">
                 <fieldset className="border border-2 border-tertiary p-4 rounded-md">
                     <legend className="text-xl font-semibold mb-2">Team Details</legend>
                     <label className="block mb-2">
@@ -45,86 +53,200 @@ const RegistrationForm = () => {
                             required
                         />
                     </label>
-                </fieldset>
-                <fieldset className="border border-gray-700 p-4 rounded-md">
-                    <legend className="text-xl font-semibold mb-2">Team Leader Details</legend>
                     <label className="block mb-2">
-                        Team Leader's Name
-                        <input
-                            type="text"
-                            name="teamLeaderName"
-                            placeholder=""
-                            value={formData.teamLeaderName}
-                            onChange={handleChange}
-                            className="w-full p-3 mt-1 border border-gray-700 rounded bg-transparent focus:outline-double focus:ring-2 focus:ring-secondary "
-                            required
-                        />
-                    </label>
-                    <label className="block mb-2">
-                        Team Leader's Email
+                        Team Email
+                        <div className='mt-1'>
+                            <span className='font-des text-sm text-neutral-400'>
+                                * Use this email as hackerank email
+                            </span>
+                        </div>
                         <input
                             type="email"
-                            name="teamLeaderEmail"
+                            name="teamEmail"
                             placeholder=""
-                            value={formData.teamLeaderEmail}
+                            value={formData.teamEmail}
                             onChange={handleChange}
                             className="w-full p-3 mt-1 border border-gray-700 rounded bg-transparent focus:outline-double focus:ring-2 focus:ring-secondary "
                             required
                         />
                     </label>
                     <label className="block mb-2">
-                        Contact Number
+                        University
                         <input
-                            type="tel"
-                            name="contactNumber"
-                            placeholder="0712345678"
-                            value={formData.contactNumber}
+                            type="text"
+                            name="university"
+                            placeholder=""
+                            value={formData.university}
                             onChange={handleChange}
                             className="w-full p-3 mt-1 border border-gray-700 rounded bg-transparent focus:outline-double focus:ring-2 focus:ring-secondary "
                             required
                         />
                     </label>
                 </fieldset>
-                <fieldset className="border border-gray-700 p-4 rounded-md">
-                    <legend className="text-xl font-semibold mb-2">Team Members</legend>
-                    <label className="block mb-2">
-                        Team Member 1 Name
-                        <input
-                            type="text"
-                            name="teamMember1"
-                            placeholder=""
-                            value={formData.teamMember1}
-                            onChange={handleChange}
-                            className="w-full p-3 mt-1 border border-gray-700 rounded bg-transparent focus:outline-double focus:ring-2 focus:ring-secondary "
-                            required
-                        />
-                    </label>
-                    <label className="block mb-2">
-                        Team Member 2 Name
-                        <input
-                            type="text"
-                            name="teamMember2"
-                            placeholder=""
-                            value={formData.teamMember2}
-                            onChange={handleChange}
-                            className="w-full p-3 mt-1 border border-gray-700 rounded bg-transparent focus:outline-double focus:ring-2 focus:ring-secondary "
-                            required
-                        />
-                    </label>
-                    <label className="block mb-2">
-                        Team Member 3 Name
-                        <input
-                            type="text"
-                            name="teamMember3"
-                            placeholder=""
-                            value={formData.teamMember3}
-                            onChange={handleChange}
-                            className="w-full p-3 mt-1 border border-gray-700 rounded bg-transparent focus:outline-double focus:ring-2 focus:ring-secondary "
-                            required
-                        />
-                    </label>
-                </fieldset>
-                <button type='submit' className="inline-flex w-full h-12 animate-shimmer items-center justify-center rounded-md border border-secondary bg-[linear-gradient(110deg,#131313,45%,#062C20,55%,#131313)] bg-[length:200%_100%] px-6 font-medium text-white tracking-widest transition-colors focus:outline-double focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+
+                <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+                    <fieldset className="border border-gray-700 p-4 rounded-md">
+                        <legend className="text-xl font-semibold mb-2">Team Leader Details</legend>
+                        <label className="block mb-2">
+                            Name
+                            <input
+                                type="text"
+                                name="teamLeaderName"
+                                placeholder=""
+                                value={formData.teamLeaderName}
+                                onChange={handleChange}
+                                className="w-full p-3 mt-1 border border-gray-700 rounded bg-transparent focus:outline-double focus:ring-2 focus:ring-secondary "
+                                required
+                            />
+                        </label>
+                        <label className="block mb-2">
+                            Email
+                            <input
+                                type="email"
+                                name="teamLeaderEmail"
+                                placeholder=""
+                                value={formData.teamLeaderEmail}
+                                onChange={handleChange}
+                                className="w-full p-3 mt-1 border border-gray-700 rounded bg-transparent focus:outline-double focus:ring-2 focus:ring-secondary "
+                                required
+                            />
+                        </label>
+                        <label className="block mb-2">
+                            Contact Number
+                            <input
+                                type="tel"
+                                name="teamleaderscontactNumber"
+                                placeholder="0712345678"
+                                value={formData.teamleaderscontactNumber}
+                                onChange={handleChange}
+                                className="w-full p-3 mt-1 border border-gray-700 rounded bg-transparent focus:outline-double focus:ring-2 focus:ring-secondary "
+                                required
+                            />
+                        </label>
+                    </fieldset>
+
+                    <fieldset className="border border-gray-700 p-4 rounded-md">
+                        <legend className="text-xl font-semibold mb-2">Team Member 01</legend>
+                        <label className="block mb-2">
+                            Name
+                            <input
+                                type="text"
+                                name="teamMember1"
+                                placeholder=""
+                                value={formData.teamMember1}
+                                onChange={handleChange}
+                                className="w-full p-3 mt-1 border border-gray-700 rounded bg-transparent focus:outline-double focus:ring-2 focus:ring-secondary "
+                                required
+                            />
+                        </label>
+                        <label className="block mb-2">
+                            Email
+                            <input
+                                type="email"
+                                name="teamMember1email"
+                                placeholder=""
+                                value={formData.teamMember1email}
+                                onChange={handleChange}
+                                className="w-full p-3 mt-1 border border-gray-700 rounded bg-transparent focus:outline-double focus:ring-2 focus:ring-secondary "
+                                required
+                            />
+                        </label>
+                        <label className="block mb-2">
+                            Contact Number
+                            <input
+                                type="tel"
+                                name="teamMember1contactNumber"
+                                placeholder="0712345678"
+                                value={formData.teamMember1contactNumber}
+                                onChange={handleChange}
+                                className="w-full p-3 mt-1 border border-gray-700 rounded bg-transparent focus:outline-double focus:ring-2 focus:ring-secondary "
+                                required
+                            />
+                        </label>
+                    </fieldset>
+
+                    <fieldset className="border border-gray-700 p-4 rounded-md">
+                        <legend className="text-xl font-semibold mb-2">Team Member 02</legend>
+                        <label className="block mb-2">
+                            Name
+                            <input
+                                type="text"
+                                name="teamMember2"
+                                placeholder=""
+                                value={formData.teamMember2}
+                                onChange={handleChange}
+                                className="w-full p-3 mt-1 border border-gray-700 rounded bg-transparent focus:outline-double focus:ring-2 focus:ring-secondary "
+                                required
+                            />
+                        </label>
+                        <label className="block mb-2">
+                            Email
+                            <input
+                                type="email"
+                                name="teamMember2email"
+                                placeholder=""
+                                value={formData.teamMember2email}
+                                onChange={handleChange}
+                                className="w-full p-3 mt-1 border border-gray-700 rounded bg-transparent focus:outline-double focus:ring-2 focus:ring-secondary "
+                                required
+                            />
+                        </label>
+                        <label className="block mb-2">
+                            Contact Number
+                            <input
+                                type="tel"
+                                name="teamMember2contactNumber"
+                                placeholder="0712345678"
+                                value={formData.teamMember2contactNumber}
+                                onChange={handleChange}
+                                className="w-full p-3 mt-1 border border-gray-700 rounded bg-transparent focus:outline-double focus:ring-2 focus:ring-secondary "
+                                required
+                            />
+                        </label>
+                    </fieldset>
+
+                    <fieldset className="border border-gray-700 p-4 rounded-md">
+                        <legend className="text-xl font-semibold mb-2">Team Member 03</legend>
+                        <label className="block mb-2">
+                            Name
+                            <input
+                                type="text"
+                                name="teamMember3"
+                                placeholder=""
+                                value={formData.teamMember3}
+                                onChange={handleChange}
+                                className="w-full p-3 mt-1 border border-gray-700 rounded bg-transparent focus:outline-double focus:ring-2 focus:ring-secondary "
+                                required
+                            />
+                        </label>
+                        <label className="block mb-2">
+                            Email
+                            <input
+                                type="email"
+                                name="teamMember3email"
+                                placeholder=""
+                                value={formData.teamMember3email}
+                                onChange={handleChange}
+                                className="w-full p-3 mt-1 border border-gray-700 rounded bg-transparent focus:outline-double focus:ring-2 focus:ring-secondary "
+                                required
+                            />
+                        </label>
+                        <label className="block mb-2">
+                            Contact Number
+                            <input
+                                type="tel"
+                                name="teamMember3contactNumber"
+                                placeholder="0712345678"
+                                value={formData.teamMember3contactNumber}
+                                onChange={handleChange}
+                                className="w-full p-3 mt-1 border border-gray-700 rounded bg-transparent focus:outline-double focus:ring-2 focus:ring-secondary "
+                                required
+                            />
+                        </label>
+                    </fieldset>
+                </div>
+
+                <button type='submit' className="inline-flex w-full h-12 animate-shimmer items-center justify-center rounded-md border border-secondary bg-[linear-gradient(110deg,#131313,45%,#062C20,55%,#131313)] bg-[length:200%_100%] px-6 font-bold text-xl text-white tracking-widest transition-colors focus:outline-double focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 font-bruno">
                     Register
                 </button>
             </form>
@@ -133,5 +255,3 @@ const RegistrationForm = () => {
 };
 
 export default RegistrationForm;
-
-
