fcab4e1b49e60832bec6d21eeec7116211c9f207
lakshith <lakshith.k.nishshanke@gmail.com>
2024-07-28 15:28:14 +0530

    add cta button hover style

--- src/components/cta.tsx
+++ src/components/cta.tsx
@@ -21,7 +21,7 @@ export function CTA() {
     },
   ];
   return (
-    <div className="flex flex-col items-center justify-center h-[40rem]  ">
+    <div className="cta flex flex-col items-center justify-center h-[40rem]  ">
       <p className="intro text-neutral-600 dark:text-neutral-200 text-xs sm:text-base  ">
         Join ReidXtreme 3.0, a premier competitive programming hackathon by the IEEE and ACM Student Chapters of UCSC. This event features two rounds: an initial qualifying round to showcase your coding skills, and a 12-hour final round hackathon for the top 10 teams. Compete, collaborate, and elevate your programming prowess in an exciting, in-person environment.
       </p>
