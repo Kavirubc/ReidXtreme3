2ca7719450816cc155763d7af02efb51f99cd5e2
Sandhavi Wanigasooriya <sandhaviwanigasooriya@gmail.com>
2024-08-13 19:24:35 +0530

    timer added

--- src/app/contactus/page.tsx
+++ src/app/contactus/page.tsx
@@ -1,59 +1,59 @@
-'use client';
+// 'use client';
 
-import { motion } from 'framer-motion';
-import React from 'react';
-import Head from 'next/head';
-import Navbar from '@/components/navbar2';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
-import Footer from '@/components/footer2';
+// import { motion } from 'framer-motion';
+// import React from 'react';
+// import Head from 'next/head';
+// import Navbar from '@/components/navbar2';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
+// import Footer from '@/components/footer2';
 
-const ContactUs = () => {
-    return (
-        <>
-            <Navbar  />
-            <main className='flex flex-col items-center justify-center text-white mt-40 mb-20 '>
-                <div className="flex flex-col items-center tracking-widest font-des md:max-w-7xl">
-                    <h1 className="text-4xl font-bold mb-12 font-bruno">Contact Us</h1>
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
-                        <motion.div
-                            whileHover={{ scale: 1.05, boxShadow: "0px 0px 20px rgba(0, 0, 0, 0.8)" }}
-                            className="flex flex-col items-center bg-transparent text-white rounded-xl shadow-lg p-6 border border-yg transition-all duration-500"
-                        >
-                            <img src="/team1.jpg" alt="contact one" className="w-32 h-32 md:w-72 md:h-72 object-contain mb-4" />
-                            <h2 className="text-xl font-bold tracking-wider mb-2">Sasmitha Silva
-                            </h2>
-                            <div className="text-white flex items-center mb-2">
-                                <FontAwesomeIcon icon={faEnvelope} className="text-yg mr-2" />
-                                <a href="mailto:silvasasmitha53@gmail.com" className="hover:underline">esilvasasmitha53@gmail.com</a>
-                            </div>
-                            <div className="text-white flex items-center">
-                                <FontAwesomeIcon icon={faPhone} className="text-yg mr-2" />
-                                <a href="tel:+94715479744" className="hover:underline">+94 71 547 9744</a>
-                            </div>
-                        </motion.div>
-                        <motion.div
-                            whileHover={{ scale: 1.05, boxShadow: "0px 0px 20px rgba(0, 0, 0, 0.8)" }}
-                            className="flex flex-col items-center bg-transparent text-white rounded-xl shadow-lg p-6 border border-yg transition-all duration-500"
-                        >
-                            <img src="/team2.jpg" alt="contact one" className="w-32 h-32 md:w-72 md:h-72 object-contain mb-4" />
-                            <h2 className="text-xl font-bold tracking-wider mb-2">Nethmi Hapuarachchi
-                            </h2>
-                            <div className="text-white flex items-center mb-2">
-                                <FontAwesomeIcon icon={faEnvelope} className="text-yg mr-2" />
-                                <a href="mailto:sathruwanihapuarachchi7@gmail.com" className="hover:underline">sathruwanihapuarachchi7@gmail.com</a>
-                            </div>
-                            <div className="text-white flex items-center">
-                                <FontAwesomeIcon icon={faPhone} className="text-yg mr-2" />
-                                <a href="tel:+94778022449" className="hover:underline">+94 77 802 2449</a>
-                            </div>
-                        </motion.div>
-                    </div>
-                </div>
-            </main>
-            <Footer />
-        </>
-    );
-};
+// const ContactUs = () => {
+//     return (
+//         <>
+//             <Navbar  />
+//             <main className='flex flex-col items-center justify-center text-white mt-40 mb-20 '>
+//                 <div className="flex flex-col items-center tracking-widest font-des md:max-w-7xl">
+//                     <h1 className="text-4xl font-bold mb-12 font-bruno">Contact Us</h1>
+//                     <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+//                         <motion.div
+//                             whileHover={{ scale: 1.05, boxShadow: "0px 0px 20px rgba(0, 0, 0, 0.8)" }}
+//                             className="flex flex-col items-center bg-transparent text-white rounded-xl shadow-lg p-6 border border-yg transition-all duration-500"
+//                         >
+//                             <img src="/team1.jpg" alt="contact one" className="w-32 h-32 md:w-72 md:h-72 object-contain mb-4" />
+//                             <h2 className="text-xl font-bold tracking-wider mb-2">Sasmitha Silva
+//                             </h2>
+//                             <div className="text-white flex items-center mb-2">
+//                                 <FontAwesomeIcon icon={faEnvelope} className="text-yg mr-2" />
+//                                 <a href="mailto:silvasasmitha53@gmail.com" className="hover:underline">esilvasasmitha53@gmail.com</a>
+//                             </div>
+//                             <div className="text-white flex items-center">
+//                                 <FontAwesomeIcon icon={faPhone} className="text-yg mr-2" />
+//                                 <a href="tel:+94715479744" className="hover:underline">+94 71 547 9744</a>
+//                             </div>
+//                         </motion.div>
+//                         <motion.div
+//                             whileHover={{ scale: 1.05, boxShadow: "0px 0px 20px rgba(0, 0, 0, 0.8)" }}
+//                             className="flex flex-col items-center bg-transparent text-white rounded-xl shadow-lg p-6 border border-yg transition-all duration-500"
+//                         >
+//                             <img src="/team2.jpg" alt="contact one" className="w-32 h-32 md:w-72 md:h-72 object-contain mb-4" />
+//                             <h2 className="text-xl font-bold tracking-wider mb-2">Nethmi Hapuarachchi
+//                             </h2>
+//                             <div className="text-white flex items-center mb-2">
+//                                 <FontAwesomeIcon icon={faEnvelope} className="text-yg mr-2" />
+//                                 <a href="mailto:sathruwanihapuarachchi7@gmail.com" className="hover:underline">sathruwanihapuarachchi7@gmail.com</a>
+//                             </div>
+//                             <div className="text-white flex items-center">
+//                                 <FontAwesomeIcon icon={faPhone} className="text-yg mr-2" />
+//                                 <a href="tel:+94778022449" className="hover:underline">+94 77 802 2449</a>
+//                             </div>
+//                         </motion.div>
+//                     </div>
+//                 </div>
+//             </main>
+//             <Footer />
+//         </>
+//     );
+// };
 
-export default ContactUs;
+// export default ContactUs;
