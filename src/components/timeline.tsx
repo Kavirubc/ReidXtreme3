fc20d02a737e6faece32d760d9a6951e381e5373
Sandhavi Wanigasooriya <sandhaviwanigasooriya@gmail.com>
2024-08-13 22:18:13 +0530

    timeline updated

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -15,26 +15,23 @@ export function Timeline() {
                 <ul className="timeline timeline-centered">
                   <li className="timeline-item">
                     <div className="timeline-info">
-                      <span>August 21, 20124</span>
+                      <span>August 17, 20124</span>
                     </div>
                     <div className="timeline-marker"></div>
                     <div className="timeline-content">
                       <h3 className="timeline-title">Workshop 1</h3>
-                      <p>Nullam vel sem. Nullam vel sem. Integer ante arcu, accumsan a,
-                        consectetuer eget, posuere ut,
-                        mauris.</p>
+                      <p>Join us for an interactive session where you'll meet the team that secured 4th place in IEEE Xtreme 17. Dive into a hands-on workshop featuring beginner-level competitive programming challenges</p>
                     </div>
                   </li>
                   <li className="timeline-item">
                     <div className="timeline-info">
-                      <span>August 23, 2024</span>
+                      <span>August 18, 2024</span>
                     </div>
                     <div className="timeline-marker"></div>
                     <div className="timeline-content">
-                      <h3 className="timeline-title">Workshop 2</h3>
-                      <p>Nullam vel sem. Nullam vel sem. Integer ante arcu,
-                        accumsan a, consectetuer eget, posuere ut,
-                        mauris. </p>
+                      <h3 className="timeline-title">Workshop 2 - Advanced Problem Solving
+                      </h3>
+                      <p>Engage in a hands-on session focused on solving medium to hard-level competitive programming questions. Learn to master problem-solving strategies and thinking patterns, guided by expert Sandaras Dhanujaya.</p>
                     </div>
                   </li>
                   <li className="timeline-item">
@@ -43,10 +40,8 @@ export function Timeline() {
                     </div>
                     <div className="timeline-marker"></div>
                     <div className="timeline-content">
-                      <h3 className="timeline-title">Initial Round</h3>
-                      <p>Nullam vel sem. Nullam vel sem. Integer ante arcu,
-                        accumsan a, consectetuer eget, posuere ut,
-                        mauris. </p>
+                      <h3 className="timeline-title">Registration Opens</h3>
+                      <p>Registrations will open on the 24th of August 2024.</p>
                     </div>
                   </li>
                   <li className="timeline-item">
@@ -55,10 +50,8 @@ export function Timeline() {
                     </div>
                     <div className="timeline-marker"></div>
                     <div className="timeline-content">
-                      <h3 className="timeline-title">Final Round Awareness Session</h3>
-                      <p>Nullam vel sem. Nullam vel sem. Integer ante arcu,
-                        accumsan a, consectetuer eget, posuere ut,
-                        mauris. </p>
+                      <h3 className="timeline-title">Initial Round</h3>
+                      <p>Test your skills in an 8-hour online competitive programming hackathon hosted on HackerRank. Compete for a place in the top 10 to secure your spot in the final round of ReidXtreme 3.0. </p>
                     </div>
                   </li>
                   <li className="timeline-item">
@@ -68,9 +61,7 @@ export function Timeline() {
                     <div className="timeline-marker"></div>
                     <div className="timeline-content">
                       <h3 className="timeline-title">Final Round</h3>
-                      <p>Nullam vel sem. Nullam vel sem. Integer ante arcu,
-                        accumsan a, consectetuer eget, posuere ut,
-                        mauris. </p>
+                      <p>The top 10 teams will meet in person to compete for the grand prizes and the title Dragonslayer. This round will be held at a physical location, which will be announced soon. </p>
                     </div>
                   </li>
                 </ul>
