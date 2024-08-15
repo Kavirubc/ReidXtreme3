1426c9d150d510f5bc34289c1e9edf5cd2143cb4
Sandhavi Wanigasooriya <sandhaviwanigasooriya@gmail.com>
2024-08-15 22:32:37 +0530

    Update image class in infinite-moving-cards.tsx and sponser.tsx

--- src/components/sponser.tsx
+++ src/components/sponser.tsx
@@ -5,33 +5,40 @@ import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
 
 export function SponserCards() {
   return (
-    <div className="h-[40rem] rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
+    <div className="h-[40rem] rounded-md flex flex-col antialiased bbg-black bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
+      <h2 className="mt-32 mb-14 text-center text-3xl font-bold font-bruno md:text-5xl">Our Partners</h2>
       <InfiniteMovingCards
         items={testimonials}
         direction="right"
-        speed="fast"
+        speed="normal"
       />
     </div>
   );
 }
 
 const testimonials = [
+  // {
+  //   image: "/wso2.png",
+  //   name: "Pearson",
+  //   title: "Gold Partner",
+  //   color: ""
+  // },
   {
-    image: "/wso2.png",
-    name: "Pearson",
-    title: "Venue Partner",
-    color: ""
-  },
-    {
     image: "/wso2.png",
     name: "WSO2",
     title: "Bronze Partner",
     color: "bronze"
   },
-    {
-    image: "/wso2.png",
+  {
+    image: "/a1.png",
     name: "Aventura",
     title: "Silver Partner",
+    color: "silver"
+  },
+  {
+    image: "/f2.png",
+    name: "Fonterra",
+    title: "Beverage Partner",
     color: "silver"
   },
 ];
