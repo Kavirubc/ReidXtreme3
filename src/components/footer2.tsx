4c34216bd5c4ebcd869b1feb558b3ab947296d8e
Sandhavi Wanigasooriya <sandhaviwanigasooriya@gmail.com>
2024-08-16 22:40:11 +0530

    social media links updated

--- src/components/footer2.tsx
+++ src/components/footer2.tsx
@@ -25,32 +25,77 @@ const Footer = () => {
                     initial={{ opacity: 0, y: 50 }}
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ duration: 0.7 }}
-                    className="flex justify-center md:gap-x-5 items-center mb-8 text-gray-700 space-x-4 md:space-x-0 md:flex-row"
+                    className="flex justify-center gap-x-6 items-center mb-8 text-gray-700 space-x-4 md:space-x-0 md:flex-row"
                 >
-                    <a
-                        href="https://www.facebook.com/ucsc.acm"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="hover:text-blue-700"
-                    >
-                        <FaFacebook className="text-white hover:text-blue-700" size={25} />
-                    </a>
-                    <a
-                        href="https://www.instagram.com/ucsc.acm/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="hover:text-pink-600"
-                    >
-                        <FaInstagram className="text-white hover:text-pink-600" size={25} />
-                    </a>
-                    <a
-                        href="https://www.linkedin.com/company/ucsc-acm/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="hover:text-cyan-500"
-                    >
-                        <FaLinkedin className="text-white hover:text-cyan-500" size={25} />
-                    </a>
+                    {/* ACM Social Media */}
+                    <div className="flex space-x-4 border-r pr-4">
+                        <a
+                            href="https://www.facebook.com/ucscacm"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="hover:text-blue-700"
+                        >
+                            <FaFacebook className="text-cyan-600 hover:text-blue-700" size={25} />
+                        </a>
+                        <a
+                            href="https://www.instagram.com/ucsc.acm"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="hover:text-pink-600"
+                        >
+                            <FaInstagram className="text-cyan-600 hover:text-pink-600" size={25} />
+                        </a>
+                        <a
+                            href="https://www.linkedin.com/company/ucscacmstudentchapter"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="hover:text-cyan-500"
+                        >
+                            <FaLinkedin className="text-cyan-600 hover:text-cyan-500" size={25} />
+                        </a>
+                    </div>
+
+                    {/* ReidXtreme Social Media */}
+                    <div className="flex space-x-4 border-r pr-4">
+                        
+                        <a
+                            href="https://www.linkedin.com/company/reidxtreme/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="hover:text-pink-600"
+                        >
+                           <FaLinkedin className="text-secondary hover:text-cyan-500" size={25} />
+                        </a>
+                        
+                    </div>
+
+                    {/* IEEE Social Media */}
+                    <div className="flex space-x-4 pr-4">
+                        <a
+                            href="https://www.facebook.com/IEEE.UCSC"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="hover:text-blue-700"
+                        >
+                            <FaFacebook className="text-sky-800 hover:text-blue-700" size={25} />
+                        </a>
+                        <a
+                            href="https://www.instagram.com/ucsc.ieee"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="hover:text-pink-600"
+                        >
+                            <FaInstagram className="text-sky-800 hover:text-pink-600" size={25} />
+                        </a>
+                        <a
+                            href="https://www.linkedin.com/company/ieee-student-branch-ucsc/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="hover:text-cyan-500"
+                        >
+                            <FaLinkedin className="text-sky-800 hover:text-cyan-500" size={25} />
+                        </a>
+                    </div>
                 </motion.div>
 
                 <motion.div
