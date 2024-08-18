c08fee867f0bbdde58e51d26510122c524e3331e
Sandhavi Wanigasooriya <sandhaviwanigasooriya@gmail.com>
2024-08-18 16:04:34 +0530

    Update meta tags for ReidXtreme 3.0 in 2024

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,48 @@
-'use client'
-import React from 'react';
+'use client';
+import React, { useState, useEffect } from 'react';
 import Prizes from '../components/prize3';
 import FAQ from '../components/faq2';
 import Footer from '../components/footer2';
-import './styles.scss'
-import Navbar from "@/components/navbar4";
+import './styles.scss';
+import Navbar from "@/components/navbar3";
 import { Hero } from "@/components/hero";
-import dragon from "../../public/hero_bg.png";
-import { CTA } from "@/components/cta2";
-import { Timeline } from "@/components/timeline";
+import { CTA } from "@/components/cta";
+import { Timeline } from "@/components/timeline2";
 import { About } from "@/components/about";
 import Contact from "@/components/contact";
 import SponserCards from "@/components/sponsor3";
-
-
+// import Register from '@/components/register';
+import Preloader from '@/components/preloading'; 
 
 export default function Home() {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 2000); 
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    if (loading) {
+        return <Preloader />;
+    }
+
     return (
         <main className={"dark"}>
             <Navbar />
             <Hero />
-            <div
-                className={"dragon"}
-            />
+            <div className={"dragon"} />
             <CTA />
             <About />
             <Timeline />
             <Prizes />
             <SponserCards />
+            {/* <Register /> */}
             <FAQ />
             <Contact />
             <Footer />
         </main>
-
-
     );
 }
