c2f296c0ae5c9bd558f193e359e1b82718f2f1cb
Sandhavi Wanigasooriya <sandhaviwanigasooriya@gmail.com>
2024-08-19 22:15:18 +0530

    new sponsors added

--- src/components/sponsor3.tsx
+++ src/components/sponsor3.tsx
@@ -3,9 +3,11 @@ import React from 'react';
 
 const SponsorCard: React.FC = () => {
     return (
-        <main className="flex flex-col min-h-screen lg:h-auto items-center justify-center text-center mt-32 p-10 lg:p-1 font-des">
-            <h2 className="mb-20 lg:mt-32 text-center text-4xl font-bold font-bruno md:text-6xl mb-14">Our Partners</h2>
-            <div className='grid grid-cols-1 lg:grid-cols-1 md:gap-14 gap-10 lg:mb-14 mb-10'>
+        <main className="flex flex-col min-h-screen lg:h-auto items-center justify-center text-center mt-64 lg:mt-24 p-6 lg-p-0 font-des">
+            <h2 className="mb-16 text-center text-4xl font-bold font-bruno md:text-6xl">Our Partners</h2>
+
+            <div className='grid grid-cols-1 lg:grid-cols-1 gap-10 lg:mb-14 mb-10'>
+            <a href="https://mooseclothingcompany.com/" target="_blank" rel="noopener noreferrer">
                 <div className="w-80 h-80 border border-yg rounded-xl bg-transparent flex flex-col items-center justify-between p-4 transform transition duration-700 hover:scale-105">
                     <div className="w-full h-60 flex items-center justify-center bg-transparent">
                         <Image src="/m3.png" alt="Moose Logo" width={200} height={75} className="object-contain" />
@@ -15,46 +17,82 @@ const SponsorCard: React.FC = () => {
                         Gold Partner
                     </div>
                 </div>
+                </a>
             </div>
-            <div className='grid grid-cols-1 lg:grid-cols-2 gap-10 lg:mb-14 mb-10'>
-                <div className="w-80 h-80 border border-gray-400 rounded-xl bg-transparent flex flex-col items-center justify-between p-4  transform transition duration-700 hover:scale-105">
-                    <div className="w-full h-60 flex items-center justify-center bg-transparent">
-                        <Image src="/a3.png" alt="Assce Logo" width={400} height={250} className="object-contain" />
-                    </div>
-                    <hr className="w-full border-t border-gray-400" />
-                    <div className="mt-5 text-des text-2xl font-bold tracking-wider text-gray-400">
-                        Silver Partner
-                    </div>
-                </div>
 
-                <div className="w-80 h-80 border border-amber-600 rounded-xl bg-transparent flex flex-col items-center justify-between p-4  transform transition duration-700 hover:scale-105">
-                    <div className="w-full h-60 flex items-center justify-center bg-transparent">
-                        <Image src="/w3.png" alt="WSO2 Logo" width={250} height={100} className="object-contain" />
-                    </div>
-                    <hr className="w-full border-t border-amber-600" />
-                    <div className="mt-5 text-des text-2xl tracking-wider font-bold text-amber-600">
-                        Bronze Partner
-                    </div>
-                </div>
 
-                <div className="w-80 h-80 border border-white rounded-xl bg-transparent flex flex-col items-center justify-between p-4  transform transition duration-700 hover:scale-105">
-                    <div className="w-full h-60 flex items-center justify-center bg-transparent">
-                        <Image src="/f3.png" alt="Fonterra Logo" width={200} height={50} className="object-contain" />
+            <div className='grid grid-cols-1 lg:grid-cols-2 gap-10 lg:gap-x-16 lg:gap-y-14'>
+                <a href="https://www.acentura.com/" target="_blank" rel="noopener noreferrer">
+                    <div className="w-full max-w-xs mx-auto border border-gray-400 rounded-xl bg-transparent flex flex-col items-center justify-between p-6 transform transition duration-500 hover:scale-105">
+                        <div className="w-full h-40 flex items-center justify-center bg-transparent">
+                            <Image src="/a3.png" alt="Accenture Logo" width={400} height={250} className="object-contain" />
+                        </div>
+                        <hr className="w-full border-t border-gray-400" />
+                        <div className="mt-4 text-des text-2xl font-bold tracking-wider text-gray-400">
+                            Silver Sponsor
+                        </div>
                     </div>
-                    <hr className="w-full border-t border-white" />
-                    <div className="mt-5 text-des text-2xl tracking-wider text-white">
-                        Beverage Partner
+                </a>
+
+                <a href="https://wso2.com/" target="_blank" rel="noopener noreferrer">
+                    <div className="w-full max-w-xs mx-auto border border-amber-600 rounded-xl bg-transparent flex flex-col items-center justify-between p-6 transform transition duration-500 hover:scale-105">
+                        <div className="w-full h-40 flex items-center justify-center bg-transparent">
+                            <Image src="/w3.png" alt="WSO2 Logo" width={250} height={100} className="object-contain" />
+                        </div>
+                        <hr className="w-full border-t border-amber-600" />
+                        <div className="mt-4 text-des text-2xl tracking-wider font-bold text-amber-600">
+                            Bronze Sponsor
+                        </div>
                     </div>
-                </div>
-                <div className="w-80 h-80 border border-white rounded-xl bg-transparent flex flex-col items-center justify-between p-4  transform transition duration-700 hover:scale-105">
-                    <div className="w-full h-60 flex items-center justify-center bg-transparent">
-                        <Image src="/g3.png" alt="Gift Logo" width={200} height={50} className="object-contain" />
+                </a>
+
+                <a href="https://zavolt.com/" target="_blank" rel="noopener noreferrer">
+                    <div className="w-full max-w-xs mx-auto border border-purple-600 rounded-xl bg-transparent flex flex-col items-center justify-between p-6 transform transition duration-500 hover:scale-105">
+                        <div className="w-full h-40 flex items-center justify-center bg-transparent">
+                            <Image src="/z3.png" alt="Zavolt Logo" width={300} height={100} className="object-contain" />
+                        </div>
+                        <hr className="w-full border-t border-purple-600" />
+                        <div className="mt-4 text-des font-bold text-2xl tracking-wider text-purple-600">
+                            Associate Sponsor
+                        </div>
                     </div>
-                    <hr className="w-full border-t border-white" />
-                    <div className="mt-5 text-des text-2xl tracking-wider text-white">
-                        Gift Partner
+                </a>
+
+                <a href="https://www.fonterra.com/lk/en.html" target="_blank" rel="noopener noreferrer">
+                    <div className="w-full max-w-xs mx-auto border border-white rounded-xl bg-transparent flex flex-col items-center justify-between p-6 transform transition duration-500 hover:scale-105">
+                        <div className="w-full h-40 flex items-center justify-center bg-transparent">
+                            <Image src="/f3.png" alt="Fonterra Logo" width={200} height={50} className="object-contain" />
+                        </div>
+                        <hr className="w-full border-t border-white" />
+                        <div className="mt-4 text-des text-2xl tracking-wider text-white">
+                            Beverage Partner
+                        </div>
                     </div>
-                </div>
+                </a>
+
+                <a href="https://linktr.ee/webcodoo" target="_blank" rel="noopener noreferrer">
+                    <div className="w-full max-w-xs mx-auto border border-white rounded-xl bg-transparent flex flex-col items-center justify-between p-6 transform transition duration-500 hover:scale-105">
+                        <div className="w-full h-40 flex items-center justify-center bg-transparent">
+                            <Image src="/g3.png" alt="Gift Logo" width={200} height={50} className="object-contain" />
+                        </div>
+                        <hr className="w-full border-t border-white" />
+                        <div className="mt-4 text-des text-2xl tracking-wider text-white">
+                            Gift Partner
+                        </div>
+                    </div>
+                </a>
+
+                <a href="https://blog.hackathons.lk/" target="_blank" rel="noopener noreferrer">
+                    <div className="w-full max-w-xs mx-auto border border-white rounded-xl bg-transparent flex flex-col items-center justify-between p-6 transform transition duration-500 hover:scale-105">
+                        <div className="w-full h-40 flex items-center justify-center bg-transparent">
+                            <Image src="/h4.png" alt="Hackathons Logo" width={350} height={100} className="object-contain" />
+                        </div>
+                        <hr className="w-full border-t border-white" />
+                        <div className="mt-4 text-des text-2xl tracking-wider text-white">
+                            Digital Media Partner
+                        </div>
+                    </div>
+                </a>
             </div>
         </main>
     );
